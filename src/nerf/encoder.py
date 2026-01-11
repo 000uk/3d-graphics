d3e52eb3e11838361029cdc0c0c46d8325ab3556
IPhone 14 pro special edition for a boy who is not only cute but also hansome <goljeol@gmail.com>
2026-01-12 00:17:35 +0900

    refactoring: PositionalEncoder 클래스 파라미터 명 변경 (x->coords)

--- src/nerf/encoder.py
+++ src/nerf/encoder.py
@@ -2,21 +2,25 @@
 import torch
 import torch.nn as nn
 
+# class Encoder:
+#     def __init__(self, x, y, z, l):
+#         self.x = x
+#         self.y = y
+
 class PositionalEncoder(nn.Module):
     def __init__(self, L=10):
         super().__init__()
         self.L = L
 
-    def forward(self, x):
-        # x는 B, 3 크기의 텐서임! x,y,z 한 번에 받도록 고침
+    def forward(self, coords): # x,y,z 한 번에 받도록 고침
         # encoded = []
-        encoded = [x]
+        encoded = [coords]
         
         for i in range(self.L):
-            t = (2.0**i) * torch.pi * x # 이거 매번 계산하면 느린가 근데
+            t = (2.0**i) * torch.pi * coords # 이거 매번 계산하면 느린가 근데
             
             encoded.append(torch.sin(t)) # math.sin은 미분 안됨
             encoded.append(torch.cos(t))
 
         # MLP에 넣을 수 있게 텐서로 변환
-        return torch.cat(encoded, dim=-1)
+        return torch.cat(encoded, dim=-1) # ([B], 63) // 1 + D*2L
