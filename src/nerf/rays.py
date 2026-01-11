8bc22211e06da774fac9c7ec7cb90f7beacf9a69
IPhone 14 pro special edition for a boy who is not only cute but also hansome <goljeol@gmail.com>
2026-01-12 02:06:32 +0900

    fix: rays_d와 rays_o 차원 불일치 수정

--- src/nerf/rays.py
+++ src/nerf/rays.py
@@ -5,7 +5,6 @@ def get_rays(H, W, focal, c2w):
     """
     각 픽셀을 카메라가 보는 dirs로 바꿈
     카메라의 회전을 적용하여 world 좌표계로 바꿈
-    r(t) = o + td에서 o랑 d를 구하는 역할임
 
     1. 픽셀 그리드 만들기
     2. 카메라 내부 좌표계로 변환
@@ -18,14 +17,15 @@ def get_rays(H, W, focal, c2w):
                           torch.arange(H, dtype=torch.float32, device=device), 
                           indexing='xy') # (H, W)
     
-    x = (i - W * .5) / focal # (H, W, 3): x,y,z가 H*W개 있음
+    x = (i - W * .5) / focal
     y = -(j - H * .5) / focal
     z = -torch.ones_like(i)
-    dirs = torch.stack([x, y, z], dim=-1)
+    dirs = torch.stack([x, y, z], dim=-1) # (H, W, 3)
     
     rays_d = dirs @ c2w[:3, :3].T
 
     # c2w: [R|t]... 모든 광선은 카메라의 위치에서 출발하겠져?
-    rays_o = c2w[:3, -1]
-    
+    rays_o = c2w[:3, -1].expand(rays_d.shape)
+    # rays_d랑 크기 맞게 expand해줌
+
     return rays_o, rays_d
