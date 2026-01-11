03243d281b628f720881e54bb6b1c50d8331c465
IPhone 14 pro special edition for a boy who is not only cute but also hansome <goljeol@gmail.com>
2026-01-12 08:39:06 +0900

    refactoring: Positional Encoding 기능의 구현을 클래스에서 함수로 변경

--- src/nerf/encoder.py
+++ src/nerf/encoder.py
@@ -1,26 +1,21 @@
 # 2. Positional Encoding
 import torch
-import torch.nn as nn
 
 # class Encoder:
 #     def __init__(self, x, y, z, l):
 #         self.x = x
 #         self.y = y
 
-class PositionalEncoder(nn.Module):
-    def __init__(self, L=10):
-        super().__init__()
-        self.L = L
+# 원래 클래스였는데 걍 함수로 바꿈 굳이 싶어서
+def positional_encode(coords, L=10): # x,y,z 한 번에 받도록 고침
+    encoded = [coords] # []
 
-    def forward(self, coords): # x,y,z 한 번에 받도록 고침
-        # encoded = []
-        encoded = [coords]
-        
-        for i in range(self.L):
-            t = (2.0**i) * torch.pi * coords # 이거 매번 계산하면 느린가 근데
-            
-            encoded.append(torch.sin(t)) # math.sin은 미분 안됨
-            encoded.append(torch.cos(t))
+    for i in range(L):
+        t = (2.0 ** i) * torch.pi * coords # 이거 매번 계산하면 느린가 근데
 
-        # MLP에 넣을 수 있게 텐서로 변환
-        return torch.cat(encoded, dim=-1) # ([B], 63) // 1 + D*2L
+        # math.sin은 미분 안됨
+        encoded.append(torch.sin(t))
+        encoded.append(torch.cos(t))
+
+    # MLP에 넣을 수 있게 텐서로 변환
+    return torch.cat(encoded, dim=-1) # ([B], 63) // 1 + D*2L
