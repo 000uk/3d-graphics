29249a7fdb48e2e21d41b65f7c41270bd44fc49e
DESKTOP-DQN8BQ4\golje <https://github.com/000uk>
2026-01-16 06:32:05 +0900

    feat: 효율적인 메모리 사용을 위해 rays 샘플링 로직 수정 (샘플링 후 get rays)

--- src/nerf/rays.py
+++ src/nerf/rays.py
@@ -12,19 +12,20 @@ def get_rays(H, W, focal, c2w):
     4. 카메라 위치도 반환
     """
     device = c2w.device
-    
-    i, j = torch.meshgrid(torch.arange(W, dtype=torch.float32, device=device), 
-                          torch.arange(H, dtype=torch.float32, device=device), 
-                          indexing='xy') # (H, W)
-    
+
+    if i is None and j is None:
+        i, j = torch.meshgrid(torch.arange(W, dtype=torch.float32, device=device),
+                              torch.arange(H, dtype=torch.float32, device=device),
+                              indexing='xy') # (H, W)
+
     x = (i - W * .5) / focal
     y = -(j - H * .5) / focal
     z = -torch.ones_like(i) # nerf는 앞이 -z임
     dirs = torch.stack([x, y, z], dim=-1) # (H, W, 3)
-    
+
     rays_d = dirs @ c2w[:3, :3].T
     rays_d = rays_d / torch.norm(rays_d, dim=-1, keepdim=True)
-    
+
     # c2w: [R|t]... 모든 광선은 카메라의 위치에서 출발하겠져?
     rays_o = c2w[:3, -1].expand(rays_d.shape)
     # rays_d랑 크기 맞게 expand해줌
